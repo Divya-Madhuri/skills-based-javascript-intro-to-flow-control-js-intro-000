6af25a3e635935a41005e2507cb1b074a97c3c9d
Divya <alaganduladivyamadhuri@gmail.com>
2019-03-27 16:26:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,15 @@
 function basicTeenager(age) {
-
-}
-
-function teenager(age) {
   if (13 < age <19) {
     console.log("You are teenager");
   }
   else {
     console.log("You are not teenager");
   }
+
+}
+
+function teenager(age) {
+
 
 }
 
